f2adff34b4fd4a0eb820367d7c3765f85f2c1eae
Chomp97 <yuri.negri2ds@gmail.com>
2022-11-03 22:06:28 +0100

    working with rands

--- Randomsheetgenerator.cpp
+++ Randomsheetgenerator.cpp
@@ -38,17 +38,92 @@ class randomSheetGenerator{
     CharSheetCofd charSheet;
     
     std::string generateRandomName(){
+        // TODO
         return "miao";
     }
 
     int generateRandomChar(){
         std::uniform_int_distribution<int> ageDistribution(8,80);
-        std::uniform_int_distribution<int> virtueDistribution(0,LastVice);
+        std::uniform_int_distribution<int> virtueDistribution(0,LastVirtue);
         std::uniform_int_distribution<int> viceDistribution(0,LastVice);
         charSheet.name(generateRandomName());
         charSheet.age(ageDistribution(gen));
         charSheet.virtue(static_cast<Virtue>(virtueDistribution(gen)));
         charSheet.vice(static_cast<Vice>(viceDistribution(gen)));
+        // randomize merits TODO after serialization of tables
+        generateRandomMerits();
+        
+    }
+
+    void generateRandomMerits(){
+        int distributions [] = {3,4,5};
+        bool doneMental,donePhysical,doneSocial = false;
+        std::uniform_int_distribution<int> attributeDistribution(1,3);
+        for(int i = 0; i < 3; i++){
+            int pool = distributions[i];
+            bool goOn = true;
+            while (goOn){
+                int pivot = attributeDistribution(gen);
+                switch (pivot){
+                    case 1:
+                        if (doneMental){
+                            continue;
+                        }
+                        assignRandomMental(pool);
+                            break;
+            
+                    case 2:
+                        if (donePhysical){
+                            continue;
+                        }
+                        assignRandomPhysical(pool);
+                            break;
+
+                    case 3:
+                        if (doneSocial){
+                            continue;
+                        }
+                        assignRandomSocial(pool);
+                            break;
+                } pivot:
+            }
+        }
+    }
+
+    void assignRandomMental(int pool){
+        bool doneInt,doneWits,doneRes = false;
+        while(pool > 0){
+            std::uniform_int_distribution<int> assignDistribution(1,pool);
+            int chunk = assignDistribution(gen);
+            pool -= chunk;
+            std::uniform_int_distribution<int> assignAttribute(1,3);
+            int pivot = assignAttribute(gen);
+            bool goOn = true;
+            while (goOn){
+                switch (pivot){
+                    case 1:
+                        break;
+            
+                    case 2:
+                        
+                        break;
+            
+                    case 3:
+                        
+                        break;
+            }
+            }
+            
+            
+        }
+    }
+
+    void assignRandomPhysical(int pool){
+
+    }
+
+    void assignRandomSocial(int pool){
+
     }
 };
 
